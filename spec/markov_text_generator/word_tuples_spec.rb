37ebfcb7ecad63d9b44254398fdacffa8da1d590
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-23 22:36:47 +0200

    Changed to new rspec syntax (allow(...) and expect(...)).

--- spec/markov_text_generator/word_tuples_spec.rb
+++ spec/markov_text_generator/word_tuples_spec.rb
@@ -5,11 +5,18 @@ module MarkovTextGenerator
     describe "scan_file" do
       before :each do
         rows = ["a b c d","e g h i"]
-        File.stub(:open).and_yield(rows)
+        allow(File).to receive(:open).and_yield(rows)
       end
   
       it "should use default file name if no argument is given" do
-        File.should_receive(:open).with("input-raw.txt", "r")
+        expect(File).to receive(:open).with("input-raw.txt", "r")
+        WordTuples.scan_file
+      end
+      
+      it "should use default number of tuples if no argument is given" do
+        default_tuples = 6
+        array = Array.new(default_tuples)
+        Array.should_receive(:new).with(6).and_return(array)
         WordTuples.scan_file
       end
     end
