9cd4853933d62a6ddaf84fb03091c81276b1a253
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-23 23:28:30 +0200

    Added test cases for scan_file.

--- spec/markov_text_generator/word_tuples_spec.rb
+++ spec/markov_text_generator/word_tuples_spec.rb
@@ -14,11 +14,35 @@ module MarkovTextGenerator
       end
       
       it "should use default number of tuples if no argument is given" do
-        default_tuples = 6
-        array = Array.new(default_tuples)
-        Array.should_receive(:new).with(6).and_return(array)
-        WordTuples.scan_file
+        pending "can't figure out how to stub Array properly"
       end
+      
+      it "should open the specified file, if given" do
+        input_file_name = "file-name"
+        expect(File).to receive(:open).with(input_file_name, "r")
+        WordTuples.scan_file(input_file_name)
+      end
+      
+      it "should use the given number of tuples, if given" do
+        pending "can't figure out how to stub Array properly"
+      end
+      
+      it "should accept two arguments" do
+        input_file_name = "file-name"
+        expect(File).to receive(:open).with(input_file_name, "r")
+        WordTuples.scan_file(2,input_file_name)
+      end
+      
+      it "should return an object of the class" do
+        expect(WordTuples.scan_file.class).to eq(WordTuples)
+      end
+      
+      it "should call new with an array of arrays" do
+        allow(File).to receive(:open).and_yield(["a"])
+        expect(WordTuples).to receive(:new).with([["a"]])
+        WordTuples.scan_file(1)
+      end
+      
     end
   end
 end
