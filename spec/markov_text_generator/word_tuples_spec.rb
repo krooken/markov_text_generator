db22ba18f443bf78f0499ffbbe0340ef7305eec9
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-23 22:12:09 +0200

    Added MarkovTextGenerator module to test spec.

--- spec/markov_text_generator/word_tuples_spec.rb
+++ spec/markov_text_generator/word_tuples_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-describe MarkovTextGenerator::WordTuples do
-  describe "scan_file" do
-    before :each do
-      rows = ["a b c d","e g h i"]
-      File.stub(:open).and_yield(rows)
-    end
+module MarkovTextGenerator
+  describe WordTuples do
+    describe "scan_file" do
+      before :each do
+        rows = ["a b c d","e g h i"]
+        File.stub(:open).and_yield(rows)
+      end
   
-    it "should use default file name if no argument is given" do
-      File.should_receive(:open).with("input-raw.txt", "r")
-      MarkovTextGenerator::WordTuples.scan_file
+      it "should use default file name if no argument is given" do
+        File.should_receive(:open).with("input-raw.txt", "r")
+        WordTuples.scan_file
+      end
     end
   end
 end
