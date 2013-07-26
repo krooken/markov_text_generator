2dc96f83e0062882d9b3b2c6cc835eb783749362
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-26 14:00:43 +0200

    Passed the tests for creting a map of maps of maps... of arays, where the arrays contain the valid words to use next after a chain of words.

--- lib/markov_text_generator/word_map.rb
+++ lib/markov_text_generator/word_map.rb
@@ -3,20 +3,31 @@ module MarkovTextGenerator
   
     def initialize(word_tuples)
       @word_map = Hash.new
-      word_tuples.each do |tuple|
+      word_tuples.each do |tuple_list|
+        tuple = tuple_list.clone
+        tuple[-1] = [tuple[-1]]
         tuple_hash = tuple.reverse.inject do |last,word|
           {word => last}
         end
         current_hash = @word_map
         key = tuple_hash.keys[0]
-        while current_hash[key].nil?
-          break
+        while tuple_hash.respond_to?(:keys) and current_hash.respond_to?(:keys) and not current_hash[key].nil?
+          tuple_hash = tuple_hash[key]
+          current_hash = current_hash[key]
+          if tuple_hash.respond_to?(:keys)
+            key = tuple_hash.keys[0]
+          end
+        end
+        if not current_hash.respond_to?(:keys) and not tuple_hash.respond_to?(:keys)
+          current_hash.push(tuple_hash).flatten!
+        elsif current_hash[key].nil?
+          current_hash[key] = tuple_hash[key]
         end
       end
     end
     
     def to_hash
-      @word_map
+      @word_map.clone
     end
     
   end
--- spec/markov_text_generator/word_map_spec.rb
+++ spec/markov_text_generator/word_map_spec.rb
@@ -4,9 +4,9 @@ module MarkovTextGenerator
   describe WordMap do
   
     before :each do
-      input_tuples = [["a","b","c"],["a","b","d"],["b","c","d"]]
+      @input_tuples = [["a","b","c"],["a","b","d"],["b","c","d"]]
       @word_tuples = double("word_tuples")
-      allow(@word_tuples).to receive(:each).and_return(input_tuples.each)
+      allow(@word_tuples).to receive(:each).and_return(@input_tuples.each)
     end
     
     describe "new" do
@@ -17,8 +17,8 @@ module MarkovTextGenerator
       end
     
       it "should create a map within map... with the words in each tuple as keys" do
-        word_map = WordMap.new(@word_tuples)
-        expect(word_map.to_hash).to eq({"a"=>{"b"=>["c","d"]},"b"=>{"c"=>"d"}})
+        word_map = WordMap.new(@input_tuples)
+        expect(word_map.to_hash).to eq({"a"=>{"b"=>["c","d"]},"b"=>{"c"=>["d"]}})
       end
   
     end
