4a952b1d18ff00626bdd0ffebd10b38086bf4626
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-26 20:56:45 +0200

    Added tests for MarkovGenerator. Require the generator to continue producing text until no word is possible afer the latest sequence of words.

--- spec/markov_text_generator/markov_generator_spec.rb
+++ spec/markov_text_generator/markov_generator_spec.rb
@@ -6,15 +6,21 @@ module MarkovTextGenerator
     describe "to_s" do
       it "should return a string" do
         generator = MarkovGenerator.new({"a"=>"b"})
-        expect(generator.class).to eq(String)
+        expect(generator.to_s.class).to eq(String)
       end
     end
     
     describe "new" do
-      it "should generate a text from the input" do
+      it "should generate a random text from the input" do
         expect(Random).to receive(:rand).and_return(0)
-        generator = MarkovGenerator.new({"a"=>{"b"=>["c","d"]},"b"=>{"c"=>["d"]}})
+        generator = MarkovGenerator.new({"a"=>{"b"=>["c","d"]}})
         expect(generator.to_s).to eq("a b c")
+      end
+      
+      it "should keep generating from the previous words" do
+        allow(Random).to receive(:rand).and_return(0)
+        generator = MarkovGenerator.new({"a"=>{"b"=>["c","d"]},"b"=>{"c"=>["e"]}})
+        expect(generator.to_s).to eq("a b c e")
       end
     end
   
