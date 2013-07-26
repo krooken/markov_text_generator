010475e06c45f49d979aa7780f8764be9633b9b9
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-26 14:23:55 +0200

    Added tests for MarkovGenerator.

--- spec/markov_text_generator/markov_generator_spec.rb
+++ spec/markov_text_generator/markov_generator_spec.rb
@@ -3,7 +3,20 @@ require 'spec_helper'
 module MarkovTextGenerator
   describe MarkovGenerator do
   
+    describe "to_s" do
+      it "should return a string" do
+        generator = MarkovGenerator.new({"a"=>"b"})
+        expect(generator.class).to eq(String)
+      end
+    end
     
+    describe "new" do
+      it "should generate a text from the input" do
+        expect(Random).to receive(:rand).and_return(0)
+        generator = MarkovGenerator.new({"a"=>{"b"=>["c","d"]},"b"=>{"c"=>["d"]}})
+        expect(generator.to_s).to eq("a b c")
+      end
+    end
   
   end
 end
