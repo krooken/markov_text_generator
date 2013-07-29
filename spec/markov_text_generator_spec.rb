ec143c7231f97a9e2ebb398442286b0ff538fd62
Jonas Krook <krook.jonas+markov_text_generator@gmail.com>
2013-07-29 13:59:10 +0200

    Added tests for creating Snowball poems.

--- spec/markov_text_generator_spec.rb
+++ spec/markov_text_generator_spec.rb
@@ -7,14 +7,31 @@ module MarkovTextGenerator
         allow(STDOUT).to receive(:puts)
         @input_file = ["a","b"]
         @output_file = ""
-        allow(File).to receive(:open).and_yield(@output_file)
+        @snowball_file = ""
+        allow(File).to receive(:open).and_yield(@output_file).and_yield(@snowball_file)
         @input_tuples = [["a","b"],["b","c"]]
         #allow(WordTuples).to receive(:new).and_return(input_tuples)
         allow(WordTuples).to receive(:scan_file).and_return(@input_tuples)
         @input_hash = {"a"=>["b"],"b"=>["c"]}
-        allow(WordMap).to receive(:new).and_return(@input_hash)
         allow(@input_tuples).to receive(:save_to_file)
         allow(@input_hash).to receive(:save_to_file)
+        @snowball_tuples = [["a","ab"],["ab","bcd"]]
+        @snowball_hash = {"a"=>["ab"],"ab"=>["bcd"]}
+        allow(@input_tuples).to receive(:filter).and_return(@snowball_tuples)
+        allow(WordMap).to receive(:new).and_return(@input_hash,@snowball_hash)
+        @markov_generator = double("markov_generator")
+        @snowball_generator = double('snowball_generator')
+        @default_generator = double('default_generator')
+        allow(MarkovGenerator).to receive(:new) do |args|
+          if args == @input_hash
+            @markov_generator 
+          elsif args == @snowball_hash
+            @snowball_generator
+          else
+            @default_generator
+          end
+        end
+        allow(@default_generator).to receive(:to_s).and_return("default")
       end
     
       it "should require a input filename and output file prefix" do
@@ -49,8 +66,18 @@ module MarkovTextGenerator
         end
       end
       
+      it "should filter out tuples where the word length are incrementing by one" do
+        MarkovTextGenerator.create_random_markov("input_file_name","output_file_name")
+        expect(@input_tuples).to have_received(:filter)
+      end
+      
       it "should create a word map from the scanned file contents" do
         expect(WordMap).to receive(:new).with(@input_tuples)
+        MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix")
+      end
+      
+      it "should create a word map from the filtered word tuples" do
+        expect(WordMap).to receive(:new).with(@snowball_tuples)
         MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix")
       end
       
@@ -59,13 +86,23 @@ module MarkovTextGenerator
         MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix")
       end
       
+      it "should create a markov generator from the snowball map" do
+        expect(MarkovGenerator).to receive(:new).with(@snowball_hash).at_least(:once)
+        MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix")
+      end
+      
       it "should write the given number of paragraphs to the output file" do
         number_of_pars = 10
-        markov_generator = double("markov_generator")
-        allow(MarkovGenerator).to receive(:new).and_return(markov_generator)
-        expect(markov_generator).to receive(:to_s).exactly(number_of_pars).times.and_return("a")
+        expect(@markov_generator).to receive(:to_s).exactly(number_of_pars).times.and_return("a")
         MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix",number_of_pars)
         expect(@output_file).to eq("a\n\na\n\na\n\na\n\na\n\na\n\na\n\na\n\na\n\na\n\n")
+      end
+      
+      it "should write the given number of poems to the output file" do
+        number_of_pars = 10
+        expect(@snowball_generator).to receive(:to_s).exactly(number_of_pars).times.and_return("s")
+        MarkovTextGenerator.create_random_markov("input_file_name","output_file_prefix",number_of_pars)
+        expect(@snowball_file).to eq("s\n\ns\n\ns\n\ns\n\ns\n\ns\n\ns\n\ns\n\ns\n\ns\n\n")
       end
       
       it "should append a string based on current time, and '.txt' to the output file" do
